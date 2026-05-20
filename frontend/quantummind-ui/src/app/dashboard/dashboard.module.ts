12760378c6171cde83ac8914c52e59238e9c62d7
Salim <doshka23@gmail.com>
2026-05-21 00:06:07 +0200

    built: DashboardModule

---
+++ frontend/quantummind-ui/src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { DashboardPage } from "./dashboard.page";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [DashboardPage],
+})
+export class DashboardModule {}
