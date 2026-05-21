06cd6385f1e69e4abc5caa6b6edd2e0d1bd5eaf5
Salim <doshka23@gmail.com>
2026-05-21 18:13:09 +0200

    built: global_exception_handler

--- backend/app/core/exceptions/global_exception_handler.py
+++ backend/app/core/exceptions/global_exception_handler.py
@@ -1,6 +1,61 @@
-from fastapi import FastAPI
+import logging
+import traceback
+from fastapi import( FastAPI, Request, HTTPException)
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import  RequestValidationError
 from app.core.settings import Settings
 
-class GlobalExceptionHandler:
+
+logger = logging.getLogger(__name__)
+
+class ExceptionsHandler:
     def __init__(self, app: FastAPI, settings: Settings):
-        pass
+        self.app = app
+        self.settings = settings
+
+    def register_handler(self):
+        self.app.exception_handler(Exception)
+
+    # =========================
+    # GLOBAL EXCEPTION
+    # =========================
+    def global_exception_handler(
+            self,
+            request: Request,
+            exc: Exception
+            ):
+        status_code: int = 500
+        status: str = "error"
+        message: str = "internal server error"
+
+        body: dict = {
+            "status": status,
+            "message": message,
+            "data": None,
+        }
+
+        if self.settings.is_dev:
+            body["stack"] = "".join(
+                traceback.format_exception(
+                    type(exc),
+                    exc,
+                    exc.__traceback__
+                )
+            )
+        logger.exception(exc)
+        return JSONResponse(
+            status_code,
+            data=body
+        )
+    
+    # =========================
+    # HTTP EXCEPTION
+    # =========================
+    async def http_global_handler(self):
+        return ""
+    
+    # =========================
+    # VALIDATION ERROR
+    # =========================
+    async def validation_handler(self):
+        return ""
