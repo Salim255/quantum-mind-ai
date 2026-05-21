fef3bbc0ce91dfe3467825520d6e15a40f5ec276
Salim <doshka23@gmail.com>
2026-05-21 14:18:12 +0200

    updated: env

--- frontend/quantummind-ui/src/environments/environment.ts
+++ frontend/quantummind-ui/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8080/api/v1' // Replace with actual production API URL
+  apiBaseUrl: 'http://127.0.0.1:8000/api/v1' // Replace with actual production API URL
   //apiBaseUrl:"https://api.zentrabank.salimcode.site/api/v1/"
 };
