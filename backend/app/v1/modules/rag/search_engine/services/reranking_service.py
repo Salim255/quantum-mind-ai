f188332763e880173f277e19c5a1dac2cb031c08
Salim <doshka23@gmail.com>
2026-05-24 18:00:01 +0200

    built:rerank_candidates

---
+++ backend/app/v1/modules/rag/search_engine/services/reranking_service.py
@@ -0,0 +1,38 @@
+from typing import List
+from app.v1.modules.rag.dto.retrieval_dto import RetrievalChunkDTO
+from app.v1.modules.rag.retriever.reranker import rerank
+
+class RerankingService:
+    @staticmethod
+    def rerank_candidates(
+        query: str,
+        candidates: List[RetrievalChunkDTO]
+        )-> List[RetrievalChunkDTO]:
+        """
+        Improve retrieval precision using cross-encoder reranking.
+
+        WHY IMPORTANT?
+        --------------
+        Dense retrieval:
+            excellent recall
+
+        Cross-encoder:
+            excellent precision
+
+        The reranker evaluates:
+            (query, chunk)
+
+        together inside the SAME transformer model.
+        """
+
+        reranked = rerank(
+            query,
+            candidates
+        )
+
+        reranked.sort(
+            key=lambda chunk: chunk.hybrid_score,
+            reverse=True
+        )
+
+        return reranked
