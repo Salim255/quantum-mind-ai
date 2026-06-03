a12edfe6114d83e4c1667ee90b6e8c25ceaf4eef
Salim <doshka23@gmail.com>
2026-06-04 00:17:43 +0200

    added: ProgressModule

---
+++ frontend/quantum-mind-ui/src/app/features/progress/progress.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { ProgressPage } from "./progress.page";
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ProgressPage]
+})
+
+export class ProgressModule {}
