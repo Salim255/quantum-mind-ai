ae97047807c69a62b25b938999798b979799bf37
Salim <doshka23@gmail.com>
2026-06-04 00:16:26 +0200

    added:  ProgressPage

---
+++ frontend/quantum-mind-ui/src/app/features/progress/progress.page.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-progress.page",
+  templateUrl: "./progress.page.html",
+  styleUrls: ["./progress.page.scss"],
+  standalone: false
+})
+
+export class ProgressPage {}
