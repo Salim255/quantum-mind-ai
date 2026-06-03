dcc0b1a51a925d2f5d27a0491ac7b251cdfa5b70
Salim <doshka23@gmail.com>
2026-06-03 23:57:15 +0200

    built: ExploreModule

---
+++ frontend/quantum-mind-ui/src/app/features/explore/explore.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { ExplorePage } from "./explore.page";
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ExplorePage]
+})
+
+export class ExploreModule {}
