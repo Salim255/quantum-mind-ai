ba51f7a699e03f1d2f73163b130029564cb2d18e
Salim <doshka23@gmail.com>
2026-06-04 00:10:40 +0200

    added: PracticeModule

---
+++ frontend/quantum-mind-ui/src/app/features/practice/practice.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { PracticePage } from "./practice.page";
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [PracticePage]
+})
+
+export class PracticeModule {}
