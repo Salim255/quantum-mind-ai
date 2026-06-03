6a4d0da796a6e9f076081d258b2752fd1af205c0
Salim <doshka23@gmail.com>
2026-06-04 00:06:25 +0200

    added: LearnRoutingModule

---
+++ frontend/quantum-mind-ui/src/app/features/learn/learn-routing.module.ts
@@ -0,0 +1,17 @@
+import { RouterModule, Routes } from "@angular/router";
+import { LearnPage } from "./learn.page";
+import { NgModule } from "@angular/core";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: LearnPage
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class LearnRoutingModule {}
