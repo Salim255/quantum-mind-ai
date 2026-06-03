baf2de80fa0847fc9c2d7f3e597cef6e3b3c07bb
Salim <doshka23@gmail.com>
2026-06-04 00:05:00 +0200

    added:  LearnModule

---
+++ frontend/quantum-mind-ui/src/app/features/learn/learn.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { LearnPage } from "./learn.page";
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [LearnPage]
+})
+export class LearnModule {}
