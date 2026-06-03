e74dd11118b62307cdff98fd57ec1297defbe822
Salim <doshka23@gmail.com>
2026-06-03 23:55:02 +0200

    built: ExplorePage

---
+++ frontend/quantum-mind-ui/src/app/features/explore/explore.page.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-explore-page",
+  templateUrl: "./explore.page.html",
+  styleUrls: ["./explore.page.scss"],
+  standalone: false
+})
+export class ExplorePage {}
