2bd9d5c756923157b1056ac8f97e175f2a7991bb
Salim <doshka23@gmail.com>
2026-06-04 00:09:29 +0200

    added: PracticePage

---
+++ frontend/quantum-mind-ui/src/app/features/practice/practice.page.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-practice-page",
+  templateUrl: "./practice.page.html",
+  styleUrls: ["./practice.page.scss"],
+  standalone: false
+})
+export class PracticePage {}
