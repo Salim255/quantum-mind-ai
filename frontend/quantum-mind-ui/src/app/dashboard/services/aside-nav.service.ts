c5c3e66a0db1553a480253a5d4098e92a7b1d98e
Salim <doshka23@gmail.com>
2026-06-05 00:29:51 +0200

    built:  PageName

--- frontend/quantum-mind-ui/src/app/dashboard/services/aside-nav.service.ts
+++ frontend/quantum-mind-ui/src/app/dashboard/services/aside-nav.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from "@angular/core";
 
+export interface LinkItem {
+  path: string;
+  name: string
+}
+export const Nav_Data = {
+  learn: {
+    links: [
+      {
+        path: "",
+        name: ""
+      }
+    ]
+  },
+  home: {},
+  explore: {},
+  practice: {},
+  progress: {},
+  resources: {}
+}
+export enum PageName {
+  LEARN="learn",
+  HOME="home",
+  EXPLORE="explore",
+  PRACTICE="practice",
+  PROGRESS="progress"
+}
+
 @Injectable({providedIn: "root"})
-export class AsideNavService {}
+export class AsideNavService {
+
+  built_links_by_page(page: PageName){
+
+  }
+}
