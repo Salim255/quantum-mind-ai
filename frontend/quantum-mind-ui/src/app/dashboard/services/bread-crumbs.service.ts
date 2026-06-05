4f03e1d0fdfa4223a45faba8508d4cf6f25d5342
Salim <doshka23@gmail.com>
2026-06-06 00:47:27 +0200

    built: buildBreadcrumbs

--- frontend/quantum-mind-ui/src/app/dashboard/services/bread-crumbs.service.ts
+++ frontend/quantum-mind-ui/src/app/dashboard/services/bread-crumbs.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { NAVIGATION } from "./data";
 
 export interface Breadcrumb {
   name: string;
@@ -8,4 +9,52 @@ export interface Breadcrumb {
 @Injectable({providedIn: 'root'})
 export class BreadcrumbService {
   constructor(){}
+
+  buildBreadcrumbs(url: string): Breadcrumb[] {
+    const breadcrumbs: Breadcrumb[] = [
+      {
+        name: 'Home',
+        path: '/home'
+      }
+    ];
+
+    for (const section of NAVIGATION) {
+
+      // Skip home because we already added it
+      if (section.path === '/home') {
+        continue;
+      }
+
+      // Match section itself
+      if (url === section.path) {
+        breadcrumbs.push({
+          name: section.name,
+          path: section.path
+        });
+
+        return breadcrumbs;
+      }
+
+      // Match child page
+      const child = section.children?.find(
+        child => child.path === url
+      );
+
+      if (child) {
+        breadcrumbs.push({
+          name: section.name,
+          path: section.path
+        });
+
+        breadcrumbs.push({
+          name: child.name,
+          path: child.path
+        });
+
+        return breadcrumbs;
+      }
+    }
+
+    return breadcrumbs;
+  }
 }
