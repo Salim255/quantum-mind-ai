123c240250289134ccb163a5cdf3266394668519
Salim <doshka23@gmail.com>
2026-06-06 00:44:44 +0200

    built: Breadcrumb

--- frontend/quantum-mind-ui/src/app/dashboard/services/bread-crumbs.service.ts
+++ frontend/quantum-mind-ui/src/app/dashboard/services/bread-crumbs.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from "@angular/core";
 
+export interface Breadcrumb {
+  name: string;
+  path: string;
+}
+
 @Injectable({providedIn: 'root'})
 export class BreadcrumbService {
   constructor(){}
