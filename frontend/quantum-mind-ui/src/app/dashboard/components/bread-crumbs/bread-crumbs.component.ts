2e973bc47f0f500fa6d94cf3b2903f5bc8477176
Salim <doshka23@gmail.com>
2026-06-06 01:27:02 +0200

    built: app breadcrumbs

--- frontend/quantum-mind-ui/src/app/dashboard/components/bread-crumbs/bread-crumbs.component.ts
+++ frontend/quantum-mind-ui/src/app/dashboard/components/bread-crumbs/bread-crumbs.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit, signal } from "@angular/core";
+import { Breadcrumb, BreadcrumbService } from "../../services/bread-crumbs.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-bread-crumbs",
@@ -6,4 +8,28 @@ import { Component } from "@angular/core";
   styleUrls: ["./bread-crumbs.component.scss"],
   standalone: false
 })
-export class BreadCrumbsComponent {}
+export class BreadCrumbsComponent implements OnInit, OnDestroy {
+  private breadCrumbsSubscription!: Subscription;
+  breadcrumbs = signal<Breadcrumb[]>([]);
+
+  constructor(private breadcrumbService: BreadcrumbService){}
+
+  ngOnInit(): void {
+    this.subscribeToBreadCrumbs();
+  }
+
+  normalizeUrl(path: string ): string{
+    return path === '/home' ? '/' : path;
+  }
+
+  subscribeToBreadCrumbs(){
+    this.breadCrumbsSubscription = this.breadcrumbService.getAppBreadCrumbs$
+    .subscribe((value: Breadcrumb[]) => {
+        console.log(value)
+        this.breadcrumbs.set(value)
+    })
+  }
+  ngOnDestroy(): void {
+    this.breadCrumbsSubscription?.unsubscribe();
+  }
+}
--- frontend/quantum-mind-ui/src/app/dashboard/dashboard.module.ts
+++ frontend/quantum-mind-ui/src/app/dashboard/dashboard.module.ts
@@ -5,10 +5,12 @@ import { DashboardRoutingModule } from "./dashboard-routing.module";
 import { HeaderComponent } from "./components/header/header.component";
 import { WrapperLayoutComponent } from "./components/wrapper-layout/wrapper-layout.component";
 import { AsideLayoutComponent } from "./components/aside-layout/aside-layout.component";
+import { BreadCrumbsComponent } from "./components/bread-crumbs/bread-crumbs.component";
 
 @NgModule({
   imports: [CommonModule, DashboardRoutingModule],
   declarations: [
+    BreadCrumbsComponent,
     AsideLayoutComponent,
     WrapperLayoutComponent,
     HeaderComponent,
