48eacfded1703d5cae8b7120bcc099ad65ce6b06
Salim <doshka23@gmail.com>
2026-06-07 13:34:04 +0200

    added: algorithms page

---
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/algorithms/algorithms.page.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-algorithms",
+  templateUrl: "./algorithms.page.html",
+  styleUrls: ["./algorithms.page.scss"]
+})
+export class AlgorithmsPage {}
