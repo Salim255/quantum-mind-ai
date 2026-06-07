8df0fbee3673221b75c5c0062e0bede66510fb71
Salim <doshka23@gmail.com>
2026-06-07 14:03:41 +0200

    built: QuantumComputingPage

---
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/quantum-computing/quantum-computing.page.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-quantum-computing",
+  templateUrl: "./quantum-computing.page.html",
+  styleUrls: ["./quantum-computing.page.scss"],
+  standalone: false
+})
+export class QuantumComputingPage {}
