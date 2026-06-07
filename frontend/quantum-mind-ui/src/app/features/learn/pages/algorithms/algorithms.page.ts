e79a70a21aa0060aba9cd20e138c45392cf074fa
Salim <doshka23@gmail.com>
2026-06-07 13:39:54 +0200

    built:AlgorithmsModule

--- frontend/quantum-mind-ui/src/app/features/learn/pages/algorithms/algorithms.page.ts
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/algorithms/algorithms.page.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-algorithms",
   templateUrl: "./algorithms.page.html",
-  styleUrls: ["./algorithms.page.scss"]
+  styleUrls: ["./algorithms.page.scss"],
+  standalone: false
 })
 export class AlgorithmsPage {}
