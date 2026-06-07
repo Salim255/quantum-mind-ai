de1fc773b6949ca97c2384fbb195d7419cabe0b5
Salim <doshka23@gmail.com>
2026-06-07 13:48:38 +0200

    built:  MathematicsModule

--- frontend/quantum-mind-ui/src/app/features/learn/pages/mathematics/mathematics.module.ts
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/mathematics/mathematics.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { MathematicsPage } from "./mathematics.page";
+import { MathematicsRoutingModule } from "./mathematics-routing.module";
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MathematicsRoutingModule],
   declarations: [MathematicsPage]
 })
 export class MathematicsModule {}
