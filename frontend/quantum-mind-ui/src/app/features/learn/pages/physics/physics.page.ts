3c9c85661ed59cafe4721e5aceae5ee4ac42b365
Salim <doshka23@gmail.com>
2026-06-07 13:50:40 +0200

    built: PhysicsPage

---
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/physics/physics.page.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-physics",
+  templateUrl: "./physics.page.html",
+  styleUrls: ["./physics.page.scss"],
+  standalone: false
+})
+export class PhysicsPage {}
