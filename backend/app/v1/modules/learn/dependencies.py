0e810aaa96cbda8b9a6bf6c90dc7d3a5fa274412
Salim <doshka23@gmail.com>
2026-06-08 20:44:57 +0200

    built:  get_topic_ingestion_service

--- backend/app/v1/modules/learn/dependencies.py
+++ backend/app/v1/modules/learn/dependencies.py
@@ -1,5 +1,10 @@
 from app.v1.modules.learn.service.learn_impl_service import LearnImplService
 from app.v1.modules.learn.service.learn_service import LearnService
+from app.v1.modules.learn.service.topic_ingestion_service import TopicIngestionService
+from app.v1.modules.learn.service.topic_ingestion_impl_service import TopicIngestionImplService
 
 def get_learn_service() -> LearnService:
     return LearnImplService()
+
+def get_topic_ingestion_service() -> TopicIngestionService:
+    return TopicIngestionImplService()
