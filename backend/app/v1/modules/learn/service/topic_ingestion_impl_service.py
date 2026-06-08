574b8d24855e27bcf3aa7af68d9f4393862a5f66
Salim <doshka23@gmail.com>
2026-06-08 20:35:35 +0200

    added:  TopicIngestionImplService

---
+++ backend/app/v1/modules/learn/service/topic_ingestion_impl_service.py
@@ -0,0 +1,5 @@
+from app.v1.modules.learn.service.topic_ingestion_service import TopicIngestionService
+
+class TopicIngestionImplService(TopicIngestionService):
+    def create_topic_from_pdf(self):
+        return ""
