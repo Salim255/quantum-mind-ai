666b7ad82bb7e19003b6baa70f6f5a13924dc194
Salim <doshka23@gmail.com>
2026-06-10 02:24:47 +0200

    all is good for next

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,1 +1,5 @@
 from app.models.topic import Topic
+from app.models.content import Content
+from app.models.document import Document
+from app.models.bookmark import Bookmark
+from app.models.section import Section
