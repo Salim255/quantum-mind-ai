c37c9a86ed53ed67a43ed5f2171ad3cdf85a44f0
Salim <doshka23@gmail.com>
2026-06-10 12:53:50 +0200

    built:  DocIngestionImplService

---
+++ backend/app/v1/modules/learn/service/doc_ingestion_impl_service.py
@@ -0,0 +1,14 @@
+from app.v1.modules.learn.service.doc_ingestion_service import DocIngestionService
+
+class DocIngestionImplService(DocIngestionService):
+    def bookmarks_extraction(self):
+        return ""
+
+    def section_extraction(self):
+        return ""
+
+    def text_extraction(self):
+        return ""
+
+    def image_extraction(self):
+        return ""
--- backend/app/v1/modules/learn/service/doc_ingestion_service.py
+++ backend/app/v1/modules/learn/service/doc_ingestion_service.py
@@ -4,7 +4,10 @@ class DocIngestionService(ABC):
     @abstractmethod
     def bookmarks_extraction(self):
         pass
-    
+
+    @abstractmethod
+    def section_extraction(self):
+        pass
     @abstractmethod
     def text_extraction(self):
         pass
