1db6ca1209e6163ff5b0d99b338d7d7135e7e783
Salim <doshka23@gmail.com>
2026-06-13 08:43:34 +0200

    built: EquationComponent

--- frontend/quantum-mind-ui/src/app/shared/components/equation/equation.component.ts
+++ frontend/quantum-mind-ui/src/app/shared/components/equation/equation.component.ts
@@ -1,8 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnChanges, signal } from "@angular/core";
+import * as katex from "katex";
 
 @Component({
   selector: "app-equation",
   templateUrl: "./equation.component.html",
   standalone: false
 })
-export class EquationComponent {}
+export class EquationComponent implements OnChanges {
+  @Input() latex = '';
+  html = signal("");
+
+   ngOnChanges(): void {
+      const equation = katex.renderToString(this.latex, {
+        throwOnError: false,
+        displayMode: true
+      });
+
+      this.html.set(equation)
+    }
+
+}
