287d3e2fcc459eb7463faddad3428cacadf3110b
Salim <doshka23@gmail.com>
2026-06-15 15:06:00 +0200

    built: learn content titles

--- frontend/quantum-mind-ui/src/app/features/learn/services/data.ts
+++ frontend/quantum-mind-ui/src/app/features/learn/services/data.ts
@@ -1,183 +1,308 @@
 
-export const SPIN =   [
+export const SPIN = [
+  {
+    name: "The Quantum Clock"
+  },
+  {
+    name: 'Measurements in the Same Direction',
+  },
+  {
+    name: 'Measurements in Different Directions'
+  },
     {
-      name: "The Quantum Clock"
-    },
-    {
-      name: 'Measurements in the Same Direction',
-    },
-    {
-      name: 'Measurements in Different Directions'
-    },
-      {
-      name: 'Measurements',
-    },
-    {
-      name: 'Randomness',
-    },
-    {
-      name: 'Photons and Polarization'
-    }
+    name: 'Measurements',
+  },
+  {
+    name: 'Randomness',
+  },
+  {
+    name: 'Photons and Polarization'
+  }
 ]
 
-export const LINEAR_ALGEBRA =   [
-    {
+export const LINEAR_ALGEBRA = [
+  {
 
-    },
-      {
-      name: 'Complex Numbers versus Real Numbers',
-    },
-      {
-      name: 'Vectors'
-    },
-      {
-      name: 'Diagrams of Vectors',
-    },
+  },
     {
-      name: 'Lengths of Vectors',
-    },
+    name: 'Complex Numbers versus Real Numbers',
+  },
     {
-      name: 'Scalar Multiplication'
-    },
+    name: 'Vectors'
+  },
     {
-      name: 'Vector Addition'
-    },
-    {
-      name: 'Orthogonal Vectors'
-    },
-    {
-      name: 'Multiplying a Bra by a Ket'
-    },
-    {
-      name: 'Bra-kets and Lengths'
-    },
-    {
-      name: 'Bra-kets and Orthogonality'
-    },
-    {
-      name: 'Orthonormal Bases'
-    },
-    {
-      name: 'Vectors as Linear Combinations of Basis Vectors'
-    },
-    {
-      name: 'Ordered Bases'
-    },
-    {
-      name: 'Length of Vectors'
-    },
-    {
-      name: 'Matrices',
-    },
-    {
-      name: 'Matrix Computations'
-    },
-    {
-      name: 'Orthogonal and Unitary Matrices'
-    },
-    {
-      name: 'Linear Algebra Toolbox'
-    }
+    name: 'Diagrams of Vectors',
+  },
+  {
+    name: 'Lengths of Vectors',
+  },
+  {
+    name: 'Scalar Multiplication'
+  },
+  {
+    name: 'Vector Addition'
+  },
+  {
+    name: 'Orthogonal Vectors'
+  },
+  {
+    name: 'Multiplying a Bra by a Ket'
+  },
+  {
+    name: 'Bra-kets and Lengths'
+  },
+  {
+    name: 'Bra-kets and Orthogonality'
+  },
+  {
+    name: 'Orthonormal Bases'
+  },
+  {
+    name: 'Vectors as Linear Combinations of Basis Vectors'
+  },
+  {
+    name: 'Ordered Bases'
+  },
+  {
+    name: 'Length of Vectors'
+  },
+  {
+    name: 'Matrices',
+  },
+  {
+    name: 'Matrix Computations'
+  },
+  {
+    name: 'Orthogonal and Unitary Matrices'
+  },
+  {
+    name: 'Linear Algebra Toolbox'
+  }
 ]
 
 // 3 Spin and Qubits
-export const SPIN_QUBITS =   [
+export const SPIN_QUBITS = [
+  {
+    name: "Probability"
+  },
+  {
+    name: 'Mathematics of Quantum Spin',
+  },
+  {
+    name: 'Equivalent State Vectors'
+  },
     {
-      name: "Probability"
-    },
+    name: 'The Basis Associated with a Given Spin Direction',
+  },
+  {
+    name: 'Rotating the Apparatus through 60°',
+  },
+  {
+    name: 'The Mathematical Model for Photon Polarization'
+  },
     {
-      name: 'Mathematics of Quantum Spin',
-    },
+    name: 'The Basis Associated with a Given Polarization Direction',
+  },
     {
-      name: 'Equivalent State Vectors'
-    },
-      {
-      name: 'The Basis Associated with a Given Spin Direction',
-    },
+    name: 'The Polarized Filters Experiments'
+  },
     {
-      name: 'Rotating the Apparatus through 60°',
-    },
+    name: 'Qubits'
+  },
     {
-      name: 'The Mathematical Model for Photon Polarization'
-    },
-     {
-      name: 'The Basis Associated with a Given Polarization Direction',
-    },
-     {
-      name: 'The Polarized Filters Experiments'
-    },
-     {
-      name: 'Qubits'
-    },
-     {
-      name: 'Alice, Bob, and Eve'
-    },
-     {
-      name: 'Probability Amplitudes and Interference'
-    },
-     {
-      name: 'Alice, Bob, Eve, and the BB84 Protocol'
-    },
-     {
-      name: 'The Mathematical Model for Photon Polarization'
-    }
+    name: 'Alice, Bob, and Eve'
+  },
+    {
+    name: 'Probability Amplitudes and Interference'
+  },
+    {
+    name: 'Alice, Bob, Eve, and the BB84 Protocol'
+  },
+    {
+    name: 'The Mathematical Model for Photon Polarization'
+  }
 ]
 
 /// 4 Entanglement
-export const ENTANGLEMENT =   [
-    {
-      name: "Alice and Bob’s Qubits Are Not Entangled"
-    },
-    {
-      name: 'Unentangled Qubits Calculation',
-    },
-    {
-      name: 'Entangled Qubits Calculation'
-    },
-    {
-      name: 'Superluminal Communication',
-    },
-    {
-      name: 'The Standard Basis for Tensor Products',
-    },
-    {
-      name: 'How Do You Entangle Qubits?'
-    },
-    {
-      name: 'Using the CNOT Gate to Entangle Qubits',
-    },
-    {
-      name: 'Entangled Quantum Clocks'
-    }
+export const ENTANGLEMENT = [
+  {
+    name: "Alice and Bob’s Qubits Are Not Entangled"
+  },
+  {
+    name: 'Unentangled Qubits Calculation',
+  },
+  {
+    name: 'Entangled Qubits Calculation'
+  },
+  {
+    name: 'Superluminal Communication',
+  },
+  {
+    name: 'The Standard Basis for Tensor Products',
+  },
+  {
+    name: 'How Do You Entangle Qubits?'
+  },
+  {
+    name: 'Using the CNOT Gate to Entangle Qubits',
+  },
+  {
+    name: 'Entangled Quantum Clocks'
+  }
 ]
 
 // Bell’s Inequality
-export const BELL_INEQUALITY =   [
-    {
-      name: "Entangled Qubits in Different Bases"
-    },
-    {
-      name: 'Einstein and Local Realism',
-    },
-    {
-      name: 'Einstein and Hidden Variables'
-    },
-    {
-      name: 'A Classical Explanation of Entanglement',
-    },
-    {
-      name: 'Bell’s Inequality',
-    },
-    {
-      name: 'The Answer of Quantum Mechanics'
-    },
-    {
-      name: 'The Classical Answer',
-    },
-    {
-      name: 'Measurement'
-    },
-    {
-      name: 'The Ekert Protocol for Quantum Key Distribution'
-    }
+export const BELL_INEQUALITY = [
+  {
+    name: "Entangled Qubits in Different Bases"
+  },
+  {
+    name: 'Einstein and Local Realism',
+  },
+  {
+    name: 'Einstein and Hidden Variables'
+  },
+  {
+    name: 'A Classical Explanation of Entanglement',
+  },
+  {
+    name: 'Bell’s Inequality',
+  },
+  {
+    name: 'The Answer of Quantum Mechanics'
+  },
+  {
+    name: 'The Classical Answer',
+  },
+  {
+    name: 'Measurement'
+  },
+  {
+    name: 'The Ekert Protocol for Quantum Key Distribution'
+  }
+]
+
+// 6 Classical Logic, Gates, and Circuits
+export const CLASSICAL_LOGIC = [
+  {
+    name: "Logic"
+  },
+  {
+    name: 'Boolean Algebra',
+  },
+  {
+    name: 'Functional Completeness'
+  },
+  {
+    name: 'Gates',
+  },
+  {
+    name: 'Circuits',
+  },
+  {
+    name: 'NAND Is a Universal Gate'
+  },
+  {
+    name: 'Gates and Computation',
+  },
+  {
+    name: 'Memory'
+  },
+  {
+    name: 'Reversible Computation'
+  },
+  {
+    name: 'Billiard Ball Computing'
+  }
+]
+
+
+// 7 Quantum Gates and Circuits
+export const QUANTUM_LOGIC = [
+  {
+    name: "Qubits"
+  },
+  {
+    name: 'The CNOT Gate',
+  },
+  {
+    name: 'Quantum Gates'
+  },
+  {
+    name: 'Quantum Gates Acting on One Qubit',
+  },
+  {
+    name: 'Are There Universal Quantum Gates?',
+  },
+  {
+    name: 'No Cloning Theorem'
+  },
+  {
+    name: 'Quantum Computation versus Classical Computation',
+  },
+  {
+    name: 'The Bell Circuit'
+  },
+  {
+    name: 'Superdense Coding'
+  },
+  {
+    name: 'Quantum Teleportation'
+  },
+  {
+    name: 'Error Correction'
+  }
+]
+
+// 8 Quantum Algorithms
+export const QUANTUM_ALGOS = [
+  {
+    name: "The Complexity Classes P and NP"
+  },
+  {
+    name: 'Are Quantum Algorithms Faster Than Classical Ones?',
+  },
+  {
+    name: 'Query Complexity'
+  },
+  {
+    name: 'Deutsch’s Algorithm',
+  },
+  {
+    name: 'The Kronecker Product of Hadamard Matrices',
+  },
+  {
+    name: 'The Deutsch-Jozsa Algorithm'
+  },
+  {
+    name: 'Simon’s Algorithm',
+  },
+  {
+    name: 'Complexity Classes'
+  },
+  {
+    name: 'Quantum Algorithms'
+  }
+]
+
+//9 Impact of Quantum Computing
+export const QUANTUM_IMPACT = [
+  {
+    name: "Shor’s Algorithm and Cryptanalysis"
+  },
+  {
+    name: 'Grover’s Algorithm and Searching Data',
+  },
+  {
+    name: 'Chemistry and Simulation'
+  },
+  {
+    name: 'Hardware',
+  },
+  {
+    name: 'Quantum Supremacy and Parallel Universes',
+  },
+  {
+    name: 'Computation'
+  },
 ]
