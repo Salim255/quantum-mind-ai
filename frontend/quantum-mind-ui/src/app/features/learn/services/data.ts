17816cf7f723f4f9b793219299c5b66f4eae7a4c
Salim <doshka23@gmail.com>
2026-06-15 13:59:17 +0200

    built: SPIN content

---
+++ frontend/quantum-mind-ui/src/app/features/learn/services/data.ts
@@ -0,0 +1,81 @@
+
+export const SPIN =   [
+    {
+      name: "The Quantum Clock"
+    },
+    {
+      name: 'Measurements in the Same Direction',
+    },
+    {
+      name: 'Measurements in Different Directions'
+    },
+      {
+      name: 'Measurements',
+    },
+    {
+      name: 'Randomness',
+    },
+    {
+      name: 'Photons and Polarization'
+    }
+]
+
+export const LINEAR_ALGEBRA =   [
+    {
+
+    },
+      {
+      name: 'Complex Numbers versus Real Numbers',
+    },
+      {
+      name: 'Vectors'
+    },
+      {
+      name: 'Diagrams of Vectors',
+    },
+    {
+      name: 'Lengths of Vectors',
+    },
+    {
+      name: 'Scalar Multiplication'
+    },
+    {
+      name: 'Vector Addition'
+    },
+    {
+      name: 'Orthogonal Vectors'
+    },
+    {
+      name: 'Multiplying a Bra by a Ket'
+    },
+    {
+      name: 'Bra-kets and Lengths'
+    },
+    {
+      name: 'Bra-kets and Orthogonality'
+    },
+    {
+      name: 'Orthonormal Bases'
+    },
+    {
+      name: 'Vectors as Linear Combinations of Basis Vectors'
+    },
+    {
+      name: 'Ordered Bases'
+    },
+    {
+      name: 'Length of Vectors'
+    },
+    {
+      name: 'Matrices',
+    },
+    {
+      name: 'Matrix Computations'
+    },
+    {
+      name: 'Orthogonal and Unitary Matrices'
+    },
+    {
+      name: 'Linear Algebra Toolbox'
+    }
+]
