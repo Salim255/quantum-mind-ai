12b5ea9000f176f495382d7fd1f06f3dcb2351d1
Salim <doshka23@gmail.com>
2026-06-15 08:24:06 +0200

    built: ClassicalLogicRoutingModule

---
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/classical-logic/classical-logic-routing.module.ts
@@ -0,0 +1,10 @@
+import { Routes } from "@angular/router";
+import { ClassicalLogicPage } from "./classical-logic.page";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: ClassicalLogicPage
+  }
+]
+export class ClassicalLogicRoutingModule {}
