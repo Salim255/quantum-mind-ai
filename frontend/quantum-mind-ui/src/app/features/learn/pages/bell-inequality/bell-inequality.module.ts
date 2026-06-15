3c6c9cc3dfcb8d832f07f932e49b71bfde560bc3
Salim <doshka23@gmail.com>
2026-06-15 08:12:54 +0200

    built: BellInequalityModule

---
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/bell-inequality/bell-inequality.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { BellInequalityPage } from "./bell-inequality.page";
+
+@NgModule({
+  imports: [],
+  declarations: [BellInequalityPage]
+})
+export class BellInequalityModule { }
