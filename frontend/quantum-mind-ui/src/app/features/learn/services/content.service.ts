73fad3856dd71b087c36bca1ebee24b20308f546
Salim <doshka23@gmail.com>
2026-06-15 17:52:11 +0200

    built: sections store

--- frontend/quantum-mind-ui/src/app/features/learn/services/content.service.ts
+++ frontend/quantum-mind-ui/src/app/features/learn/services/content.service.ts
@@ -3,13 +3,20 @@ import { BehaviorSubject, Observable } from "rxjs";
 
 @Injectable({providedIn: "root"})
 export class ContentService {
-  private currentSectionsSubject = new BehaviorSubject< { name: string } []>([]) ;
+  private currentSectionsSubject = new BehaviorSubject< { name: string } []>(
+    JSON.parse(localStorage.getItem('asideSections') ?? '[]')
+  );
 
-  setPageAsideContent(sections: { name: string } []){
+  setPageAsideContent(sections: { name: string } []) {
+    localStorage.setItem("asideSections",JSON.stringify(sections));
     this.currentSectionsSubject.next(sections);
   }
 
   get getPageAsideContent$(): Observable<{ name: string } []>{
     return this.currentSectionsSubject.asObservable()
+  }
+
+  clearStorage(): void{
+    localStorage.removeItem("asideSections")
   }
 }
