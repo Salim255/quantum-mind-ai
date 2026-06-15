7ea95b02731352dedf36ddf4fef528c09d016957
Salim <doshka23@gmail.com>
2026-06-15 08:00:00 +0200

    built:  EntanglementPage

---
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/entanglement/entanglement.page.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-entanglement-page",
+  templateUrl: "./entanglement.page.html",
+  styleUrl: "./entanglement.page.scss",
+  standalone: false
+})
+export class EntanglementPage {}
