04df8f1ee12e407631bd903fd65121adecb9f3d0
Salim <doshka23@gmail.com>
2026-06-15 07:56:55 +0200

    built: EntanglementModule

---
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/entanglement/entanglement.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { EntanglementRoutingModule } from "./entanglement-routing.module";
+
+@NgModule({
+  imports: [CommonModule, EntanglementRoutingModule],
+  declarations: []
+})
+export class EntanglementModule {}
