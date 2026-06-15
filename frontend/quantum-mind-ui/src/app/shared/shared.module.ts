5e6e4aa9472aea8b3fe9860c84a8f891f67dee82
Salim <doshka23@gmail.com>
2026-06-15 07:06:29 +0200

    built: page content aside style

--- frontend/quantum-mind-ui/src/app/shared/shared.module.ts
+++ frontend/quantum-mind-ui/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { EquationComponent } from "./components/equation/equation.component";
+import { PageContentAsideComponent } from "./components/page-content-aside/page-content-aside.component";
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [EquationComponent],
-  exports: [EquationComponent]
+  declarations: [PageContentAsideComponent, EquationComponent],
+  exports: [PageContentAsideComponent, EquationComponent]
 })
 export class SharedModule {}
