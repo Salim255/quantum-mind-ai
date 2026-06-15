eaa8e813cde1f8813268bba801f31a0e3a3a5efc
Salim <doshka23@gmail.com>
2026-06-15 07:30:23 +0200

    built: spin page

--- frontend/quantum-mind-ui/src/app/features/learn/pages/spin/spin-routing.module.ts
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/spin/spin-routing.module.ts
@@ -1,9 +1,10 @@
 import { Routes } from "@angular/router";
+import { SpinPage } from "./spin.page";
 
 const routes: Routes = [
   {
     path: "",
-    component:
+    component: SpinPage
 
   }
 ]
---
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/spin/spin.page.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-spin-page",
+  templateUrl: "./spin.page.html",
+  styleUrl: "./spin.page.scss",
+  standalone: false
+})
+export class SpinPage {}
