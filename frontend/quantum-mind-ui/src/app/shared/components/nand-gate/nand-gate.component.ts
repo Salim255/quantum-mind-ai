7148a5171c35751897da75b295d9e77e1e76a2b2
Salim <doshka23@gmail.com>
2026-06-17 00:50:59 +0200

    built: NandGateComponent

---
+++ frontend/quantum-mind-ui/src/app/shared/components/nand-gate/nand-gate.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-nand-gate",
+  templateUrl: "./nand-gate.component.html",
+  styleUrl: "./nand-gate.component.scss",
+  standalone: false
+})
+export class NandGateComponent {}
