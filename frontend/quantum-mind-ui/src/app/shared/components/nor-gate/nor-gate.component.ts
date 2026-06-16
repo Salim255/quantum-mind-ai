0322965f10b3109870a416cd989580707088a77e
Salim <doshka23@gmail.com>
2026-06-17 00:55:55 +0200

    built: NorGateComponent

---
+++ frontend/quantum-mind-ui/src/app/shared/components/nor-gate/nor-gate.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-nor-gate",
+  templateUrl: "./nor-gate.component.html",
+  styleUrl: "./nor-gate.component.scss",
+  standalone: false
+})
+export class NorGateComponent {}
