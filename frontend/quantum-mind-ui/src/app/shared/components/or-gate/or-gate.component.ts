f99cba079f6e314953dac4e1b933ccc02b87f3aa
Salim <doshka23@gmail.com>
2026-06-17 00:48:56 +0200

    buit: OrGateComponent

---
+++ frontend/quantum-mind-ui/src/app/shared/components/or-gate/or-gate.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-or-gate",
+  templateUrl: "./or-gate.component.html",
+  styleUrl: "./or-gate.component.scss",
+  standalone: false
+})
+export class OrGateComponent {}
