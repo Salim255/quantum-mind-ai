8fb52d9ac57f6a511b252a86a713bf745950d5c5
Salim <doshka23@gmail.com>
2026-06-17 01:12:22 +0200

    added: assets

--- frontend/quantum-mind-ui/src/app/shared/shared.module.ts
+++ frontend/quantum-mind-ui/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { EquationComponent } from "./components/equation/equation.component";
 import { PageContentAsideComponent } from "./components/page-content-aside/page-content-aside.component";
+import { AndGateComponent } from "./components/and-gate/and-gate.component";
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [PageContentAsideComponent, EquationComponent],
-  exports: [PageContentAsideComponent, EquationComponent]
+  declarations: [AndGateComponent, PageContentAsideComponent, EquationComponent],
+  exports: [AndGateComponent, PageContentAsideComponent, EquationComponent]
 })
 export class SharedModule {}
