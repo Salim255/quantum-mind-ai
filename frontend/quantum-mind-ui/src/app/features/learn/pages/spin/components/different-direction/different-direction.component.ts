e0f69db7405456f4db65e2b00898714eb7630ca5
Salim <doshka23@gmail.com>
2026-06-17 17:32:07 +0200

    buit: DifferentDirectionComponent

---
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/spin/components/different-direction/different-direction.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-different-direction",
+  templateUrl: "./different-direction.component.html",
+  styleUrl: "./different-direction.component.scss",
+  standalone: false
+})
+export class DifferentDirectionComponent {}
