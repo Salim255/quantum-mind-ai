3ce6017151dc9c2b5723ce3c63bbfc4ca8306a61
Salim <doshka23@gmail.com>
2026-06-17 16:04:27 +0200

    built: SpinHomeComponent

---
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/spin/components/spin-home/spin-home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-spin-home",
+  templateUrl: "./spin-home.component.html",
+  styleUrl: "./spin-home.component.scss",
+  standalone: false
+})
+export class SpinHomeComponent {}
