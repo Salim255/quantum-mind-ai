9af4a13b7a44da53aa4f766ffb3c366b88d15ccd
Salim <doshka23@gmail.com>
2026-06-17 18:03:35 +0200

    built: RandomnessComponent

---
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/spin/components/randomness/randomness.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-randomness",
+  templateUrl: "./randomness.component.html",
+  styleUrl: "./randomness.component.scss",
+  standalone: false
+})
+export class RandomnessComponent {}
--- frontend/quantum-mind-ui/src/app/features/learn/pages/spin/spin.module.ts
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/spin/spin.module.ts
@@ -6,10 +6,12 @@ import { SpinHomeComponent } from "./components/spin-home/spin-home.component";
 import { QuantumClockComponent } from "./components/quantum-clock/quantum-clock.component";
 import { SamDirectionComponent } from "./components/same-direction/same-direction.component";
 import { DifferentDirectionComponent } from "./components/different-direction/different-direction.component";
+import { MeasurementComponent } from "./components/measurement/measurement.component";
 
 @NgModule({
   imports: [CommonModule, SpinRoutingModule],
   declarations: [
+    MeasurementComponent,
     DifferentDirectionComponent,
     SamDirectionComponent,
     QuantumClockComponent,
