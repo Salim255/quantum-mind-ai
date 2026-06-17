a7a7551f77ede1037f0186e5ad5ceb9660d7e568
Salim <doshka23@gmail.com>
2026-06-17 16:32:30 +0200

    built: QuantumClockComponent

---
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/spin/components/quantum-clock/quantum-clock.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-quantum-clock",
+  templateUrl: "./quantum-clock.component.html",
+  styleUrl: "./quantum-clock.component.scss",
+  standalone: false
+})
+export class QuantumClockComponent {}
