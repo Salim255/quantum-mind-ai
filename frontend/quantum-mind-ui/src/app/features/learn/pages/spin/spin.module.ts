721722eb81e405e1becb916eeab519ab4b1b252d
Salim <doshka23@gmail.com>
2026-06-17 16:20:31 +0200

    built: spin home summary

--- frontend/quantum-mind-ui/src/app/features/learn/pages/spin/spin.module.ts
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/spin/spin.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { SpinPage } from "./spin.page";
 import { SpinRoutingModule } from "./spin-routing.module";
+import { SpinHomeComponent } from "./components/spin-home/spin-home.component";
 
 @NgModule({
   imports: [CommonModule, SpinRoutingModule],
-  declarations: [SpinPage]
+  declarations: [SpinHomeComponent, SpinPage]
 })
 export class SpinModule {}
