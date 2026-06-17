632b8519679f3bb54f0386be76e8f9897a530bda
Salim <doshka23@gmail.com>
2026-06-17 16:47:54 +0200

    built: SamDirectionComponent

---
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/spin/components/same-direction/same-direction.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-same-direction",
+  templateUrl: "./same-direction.component.html",
+  styleUrl: "./same-direction.component.scss",
+  standalone: false
+})
+export class SamDirectionComponent {}
--- frontend/quantum-mind-ui/src/app/features/learn/pages/spin/spin.module.ts
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/spin/spin.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 import { SpinPage } from "./spin.page";
 import { SpinRoutingModule } from "./spin-routing.module";
 import { SpinHomeComponent } from "./components/spin-home/spin-home.component";
+import { QuantumClockComponent } from "./components/quantum-clock/quantum-clock.component";
 
 @NgModule({
   imports: [CommonModule, SpinRoutingModule],
-  declarations: [SpinHomeComponent, SpinPage]
+  declarations: [QuantumClockComponent, SpinHomeComponent, SpinPage]
 })
 export class SpinModule {}
