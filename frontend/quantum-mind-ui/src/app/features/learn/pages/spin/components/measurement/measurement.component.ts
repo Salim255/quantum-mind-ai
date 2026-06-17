e32728c2d6641ed80bfb760c984254af4f540119
Salim <doshka23@gmail.com>
2026-06-17 17:45:59 +0200

    built: MeasurementComponent

---
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/spin/components/measurement/measurement.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-measurement",
+  templateUrl: "./measurement.component.html",
+  styleUrl: "./measurement.component.scss",
+  standalone: false
+})
+export class MeasurementComponent {}
