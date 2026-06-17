ec9726043162452ce1875c6ab752f6c5c7014670
Salim <doshka23@gmail.com>
2026-06-17 18:17:54 +0200

    built: PhotonsPolarizationComponent

---
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/spin/components/photons-polarization/photons-polarization.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-photons-polarization",
+  templateUrl: "./photons-polarization.component.html",
+  styleUrl: "./photons-polarization.component.scss",
+  standalone: false
+})
+export class PhotonsPolarizationComponent {}
