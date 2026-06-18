08056423b4d65a6c40369b9058b6add5a468dccf
Salim <doshka23@gmail.com>
2026-06-18 14:23:06 +0200

    updated: get_doc_ingestion_service

--- backend/app/v1/modules/ingestion/dependencies.py
+++ backend/app/v1/modules/ingestion/dependencies.py
@@ -1,6 +1,16 @@
+from typing import Annotated
 from app.v1.modules.ingestion.service.ingestion_impl_service import DocIngestionImplService
 from app.v1.modules.ingestion.service.ingestion_service import DocIngestionService
+from app.core.container import Container
+from fastapi import Request, Depends
 
+# ------------------------------------------------------------
+# CONTAINER DEPENDENCY
+# ------------------------------------------------------------
+def get_container(request: Request) -> Container:
+    return request.app.state.container
 
-def get_doc_ingestion_service() -> DocIngestionService:
-    return DocIngestionImplService()
+def get_doc_ingestion_service(
+        container: Annotated[Container, Depends(get_container)]
+    ) -> DocIngestionService:
+    return DocIngestionImplService(container=container)
