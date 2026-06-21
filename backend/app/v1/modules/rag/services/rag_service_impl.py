3fa1b63bd5cc1493afb8433b9c875c5a468742da
Salim <doshka23@gmail.com>
2026-06-21 11:27:43 +0200

    refacotred: rag services

--- backend/app/v1/modules/rag/services/rag_service_impl.py
+++ backend/app/v1/modules/rag/services/rag_service_impl.py
@@ -4,9 +4,9 @@ from pydantic import BaseModel
 from app.v1.modules.rag.context.context_builder import build_reasoned_context
 from app.v1.modules.rag.dto.rag_finale_response_dto import RAGQueryFinaleResponseDto
 from app.v1.modules.rag.services.rag_service import RAGService
-from app.v1.modules.rag.generator.generator_service import (generate_answer, generate_streaming_answer)
+from app.v1.modules.rag.services.generator.generator_service import (generate_answer, generate_streaming_answer)
 from app.v1.modules.rag.dto.rag_eval_dto import RAGEvaluationLogDto
-from app.v1.modules.rag.evaluation.logger import log_rag_evaluation
+from app.v1.modules.rag.services.evaluation.logger import log_rag_evaluation
 from app.v1.modules.rag.dto.retrieval_dto import (RetrievalResponseDTO, RetrievalChunkDTO)
 from app.v1.modules.rag.services.retriever.retriever_engine_impl import RetrieverImpl
 from app.core.container import Container
