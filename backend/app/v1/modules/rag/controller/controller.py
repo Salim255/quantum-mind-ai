91073c227e67786ce403990a55f4e8099c24d092
Salim <doshka23@gmail.com>
2026-06-21 09:46:22 +0200

    delete: conversation controller

--- backend/app/v1/modules/rag/controller/controller.py
+++ backend/app/v1/modules/rag/controller/controller.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 from app.v1.modules.rag.dto.rag_finale_response_dto import RAGQueryFinaleResponseDto
 from app.v1.modules.rag.services.interfaces.rag_service import RAGService
 from app.v1.modules.rag.dependencies import get_rag_service
+from app.v1.modules.rag.dto.conversation_dto import (ConversationRequest, ConversationResponse)
 
 router = APIRouter(
     prefix="/rag",
@@ -13,6 +14,48 @@ router = APIRouter(
 class QueryRequest(BaseModel):
     query: str
     top_k: int = 3
+
+async def send_message(
+    payload: ConversationRequest,
+    conversation_service: Annotated[ConversationService, Depends(get_conversation_service)]
+) -> ResponseDTO:
+    response: ConversationResponse = await conversation_service.handle_message(
+        user_id=payload.user_id,
+        message=payload.message,
+        conversation_id=payload.conversation_id
+    )
+    return ResponseDTO(data=response)
+
+
+@router.post(
+    "/messages/stream",
+    status_code=200,
+     response_class=StreamingResponse,
+    response_description="Streams AI response chunks",
+    responses={
+        500: {"description": "Streaming failed"}
+    }
+)
+async def stream_message(
+    payload: ConversationRequest,
+    conversation_service: Annotated[
+        ConversationService,
+        Depends(get_conversation_service)
+    ]
+)-> StreamingResponse:
+    # ------------------------------------------------------
+    # STREAM EVENTS FROM SERVICE
+    # ------------------------------------------------------
+    event_generator: Generator[str, None, None] = conversation_service.stream_message(
+        user_id=payload.user_id,
+        message=payload.message,
+        conversation_id=payload.conversation_id
+    )
+
+    return StreamingResponse(
+        event_generator,
+        media_type="text/event-stream"
+        )
 
 @router.post("/query")
 def rag_query(
