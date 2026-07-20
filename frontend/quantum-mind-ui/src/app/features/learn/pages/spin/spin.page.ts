42c7ee30b81aa4318126d6a86c2b9dd01c3c8279
Salim <doshka23@gmail.com>
2026-07-20 17:55:09 +0200

    built: spin scroll page

--- frontend/quantum-mind-ui/src/app/features/learn/pages/spin/spin.page.ts
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/spin/spin.page.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, OnDestroy, QueryList, ViewChildren } from "@angular/core";
 import { Subscription } from "rxjs";
 import { PageAsideService } from "../../../../shared/service/page-aside-content.service";
 
@@ -8,6 +8,50 @@ import { PageAsideService } from "../../../../shared/service/page-aside-content.
   styleUrl: "./spin.page.scss",
   standalone: false
 })
-export class SpinPage {
+export class SpinPage  implements AfterViewInit, OnDestroy {
+  @ViewChildren('pageSection')
+  private sections!: QueryList<ElementRef<HTMLElement>>;
+  private observer?: IntersectionObserver;
+
   constructor(private pageAsideService: PageAsideService){}
+
+  ngAfterViewInit(): void {
+    this.observeSections();
+  }
+
+    private observeSections(): void {
+
+      this.observer = new IntersectionObserver(
+        entries => {
+
+            const visibleEntry = entries.find(
+              entry => entry.isIntersecting
+            );
+
+            if (!visibleEntry) {
+              return;
+            }
+            this.pageAsideService.setCurrentSectionId(
+              visibleEntry.target.id
+            );
+        },
+
+        {
+          root: null,
+          rootMargin: "-80px 0px -60% 0px",
+          threshold: 0
+        }
+
+      );
+
+      this.sections?.forEach(section => {
+        this.observer!.observe(
+          section.nativeElement
+        );
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.observer?.disconnect();
+  }
 }
