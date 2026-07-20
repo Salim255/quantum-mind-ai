35387697eefc6441d4ec3b7a221bff8ef118bfad
Salim <doshka23@gmail.com>
2026-07-21 00:55:37 +0200

    built: QuizMultipleChoiceComponent

---
+++ frontend/quantum-mind-ui/src/app/features/practice/pages/components/quiz-multiple-choice/quiz-multiple-choice.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-quiz-multiple-choice",
+  templateUrl: "./quiz-multiple-choice.component.html",
+  styleUrls: ["./quiz-multiple-choice.component.scss"],
+  standalone: false
+})
+export class QuizMultipleChoiceComponent {}
--- frontend/quantum-mind-ui/src/app/features/practice/pages/quiz-page/quiz.module.ts
+++ frontend/quantum-mind-ui/src/app/features/practice/pages/quiz-page/quiz.module.ts
@@ -4,10 +4,16 @@ import { QuizRoutingModule } from "./quiz-routing.module";
 import { QuizPage } from "./quiz-page";
 import { QuizHeaderComponent } from "../components/quiz-header/quiz-header.component";
 import { QuizProgressComponent } from "../components/quiz-progress/quiz-progress.component";
+import { QuizQuestionComponent } from "../components/quiz-question/quiz-question.component";
 
 @NgModule({
   imports: [QuizRoutingModule, CommonModule],
-  declarations: [QuizPage, QuizHeaderComponent, QuizProgressComponent],
+  declarations: [
+    QuizPage,
+    QuizHeaderComponent,
+    QuizProgressComponent,
+    QuizQuestionComponent
+  ],
 
 })
 export class QuizModule{}
