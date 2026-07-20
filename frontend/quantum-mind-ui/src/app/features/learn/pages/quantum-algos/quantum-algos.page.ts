9d97da81ab36db2789e526c74f68083c019b0cba
Salim <doshka23@gmail.com>
2026-07-20 16:21:21 +0200

    built: qunatum algo scroll system

--- frontend/quantum-mind-ui/src/app/features/learn/pages/quantum-algos/quantum-algos.page.ts
+++ frontend/quantum-mind-ui/src/app/features/learn/pages/quantum-algos/quantum-algos.page.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, QueryList, ViewChildren } from "@angular/core";
+import { PageAsideService } from "../../../../shared/service/page-aside-content.service";
 
 @Component({
   selector: "app-quantum-algos-page",
@@ -6,4 +7,50 @@ import { Component } from "@angular/core";
   styleUrl: "./quantum-algos.page.scss",
   standalone: false
 })
-export class QuantumAlgosPage {}
+export class QuantumAlgosPage {
+  @ViewChildren('pageSection')
+  private sections!: QueryList<ElementRef<HTMLElement>>;
+  private observer?: IntersectionObserver;
+
+  constructor(private pageAsideService: PageAsideService){}
+
+  ngAfterViewInit(): void {
+    this.observeSections();
+  }
+
+    private observeSections(): void {
+
+      this.observer = new IntersectionObserver(
+        entries => {
+
+            const visibleEntry = entries.find(
+              entry => entry.isIntersecting
+            );
+
+            if (!visibleEntry) {
+              return;
+            }
+            this.pageAsideService.setCurrentSectionId(
+              visibleEntry.target.id
+            );
+        },
+
+        {
+          root: null,
+          rootMargin: "-80px 0px -60% 0px",
+          threshold: 0
+        }
+
+      );
+
+      this.sections?.forEach(section => {
+        this.observer!.observe(
+          section.nativeElement
+        );
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.observer?.disconnect();
+  }
+}
