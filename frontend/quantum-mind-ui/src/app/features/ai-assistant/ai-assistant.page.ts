9c4197dedc0ebfc05408e10c0b6e1fb0a1b03865
Salim <doshka23@gmail.com>
2026-08-11 19:41:49 +0200

    built:  AIAssistantPage

---
+++ frontend/quantum-mind-ui/src/app/features/ai-assistant/ai-assistant.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ai-assistant-page',
+  templateUrl: './ai-assistant.page.html',
+  styleUrl: './ai-assistant.page.scss',
+  standalone: false
+})
+export class AIAssistantPage {}
