dd00bcb0354726c2b47df81115ac9ae27f2838ad
Salim <doshka23@gmail.com>
2026-08-20 17:46:17 +0200

    built: progress header style

--- frontend/quantum-mind-ui/src/app/features/progress/components/progress-header/progress-header.component.ts
+++ frontend/quantum-mind-ui/src/app/features/progress/components/progress-header/progress-header.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress-header',
-  standalone: true,
+  standalone: false,
   templateUrl: './progress-header.component.html',
   styleUrl: './progress-header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- frontend/quantum-mind-ui/src/app/features/progress/progress.module.ts
+++ frontend/quantum-mind-ui/src/app/features/progress/progress.module.ts
@@ -2,10 +2,14 @@ import { CommonModule } from "@angular/common";
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
 import { ProgressPage } from "./progress.page";
 import { ProgressRoutingModule } from "./progress-routing.module";
+import { ProgressHeaderComponent } from "./components/progress-header/progress-header.component";
 
 @NgModule({
   imports: [CommonModule, ProgressRoutingModule],
-  declarations: [ProgressPage],
+  declarations: [
+    ProgressHeaderComponent,
+    ProgressPage
+  ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA
   ]
