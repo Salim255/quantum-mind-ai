00d999204513e22def149d78318d4f5784d5d91c
Salim <doshka23@gmail.com>
2026-08-20 17:35:20 +0200

    built: ProgressHeaderComponent

---
+++ frontend/quantum-mind-ui/src/app/features/progress/components/progress-header/progress-header.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-progress-header',
+  standalone: true,
+  templateUrl: './progress-header.component.html',
+  styleUrl: './progress-header.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProgressHeaderComponent {}
