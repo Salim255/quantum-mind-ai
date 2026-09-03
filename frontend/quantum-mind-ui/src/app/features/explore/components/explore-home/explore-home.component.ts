eb0fad3f17c9dffddb20a27dea0ed896479c83be
Salim <doshka23@gmail.com>
2026-09-03 15:13:49 +0200

    built: ExploreHomeComponent

---
+++ frontend/quantum-mind-ui/src/app/features/explore/components/explore-home/explore-home.component.ts
@@ -0,0 +1,45 @@
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { ExploreService } from '../../services/explore.service';
+import { Subscription } from 'rxjs';
+import { Topic } from '../../models/topic.model';
+
+
+@Component({
+  selector: 'app-explore-home',
+  templateUrl: './explore-home.component.html',
+  styleUrls: ['./explore-home.component.scss'],
+  standalone: false,
+})
+export class ExploreHomeComponent implements OnInit, OnDestroy {
+
+   private topicsSubscription?: Subscription;
+
+  readonly topics = signal<Topic[]>([]);
+
+  constructor(
+    private readonly exploreService: ExploreService,
+  ) {}
+
+  ngOnInit(): void {
+    this.subscribeToTopics();
+    this.exploreService.fetchTopics();
+  }
+
+  private subscribeToTopics(): void {
+    this.topicsSubscription = this.exploreService
+      .getTopics$()
+      .subscribe(topics => {
+        this.topics.set(topics);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.topicsSubscription?.unsubscribe();
+  }
+
+}
--- frontend/quantum-mind-ui/src/app/features/explore/explore.module.ts
+++ frontend/quantum-mind-ui/src/app/features/explore/explore.module.ts
@@ -4,10 +4,12 @@ import { ExplorePage } from "./explore.page";
 import { ExploreRoutingModule } from "./explore-routing.module";
 import { ExploreTopicComponent } from "./components/explore-topic/explore-topic.component";
 import { ExploreHeaderComponent } from "./components/explore-header/explore-header.component";
+import { ExploreHomeComponent } from "./components/explore-home/explore-home.component";
 
 @NgModule({
   imports: [CommonModule, ExploreRoutingModule],
   declarations: [
+    ExploreHomeComponent,
     ExploreHeaderComponent,
     ExploreTopicComponent,
     ExplorePage
