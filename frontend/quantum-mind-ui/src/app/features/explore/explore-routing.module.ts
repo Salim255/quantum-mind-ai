d2f1edbff04cb410e4eec7f2aec250f20bd22a0b
Salim <doshka23@gmail.com>
2026-09-03 15:33:43 +0200

    built: explore-home style

--- frontend/quantum-mind-ui/src/app/features/explore/explore-routing.module.ts
+++ frontend/quantum-mind-ui/src/app/features/explore/explore-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { ExplorePage } from "./explore.page";
+import { ExploreHomeComponent } from "./components/explore-home/explore-home.component";
 
 const routes: Routes = [
   {
     path: "",
-    component: ExplorePage
+    component: ExplorePage,
+    children: [
+      {
+        path: "",
+        component: ExploreHomeComponent
+      },
+    
+    ]
   }
 ]
 
