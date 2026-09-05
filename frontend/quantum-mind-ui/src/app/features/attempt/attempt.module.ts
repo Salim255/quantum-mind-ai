ae3cfcf2f0f77f644d193fa26d3347d771f255b9
Salim <doshka23@gmail.com>
2026-09-05 14:47:18 +0200

    built:attempt-header

--- frontend/quantum-mind-ui/src/app/features/attempt/attempt.module.ts
+++ frontend/quantum-mind-ui/src/app/features/attempt/attempt.module.ts
@@ -2,13 +2,17 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
 import { AttemptPage } from "./attempt.page";
 import { AttemptRoutingModule } from "./attempt-routing.module";
 import { CommonModule } from "@angular/common";
+import { AttemptHeaderComponent } from "./components/attempt-header/attempt-header.component";
 
 @NgModule({
   imports: [
     CommonModule,
     AttemptRoutingModule,
   ],
-  declarations: [AttemptPage],
+  declarations: [
+    AttemptHeaderComponent,
+    AttemptPage
+  ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA
   ]
